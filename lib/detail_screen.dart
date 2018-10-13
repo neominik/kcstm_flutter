d16bd67bc8b42df17cb8c7f27dd61af51e190657
Dominik Engelhardt <Dominik.Engelhardt@dlr.de>
2018-10-13 12:34:51 +0200

    Add button to participate

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -9,45 +9,57 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Details'),
+      ),
+      body: _buildDetails(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Mitfahren',
+        child: Icon(Icons.group_add),
+      ),
+    );
+  }
+
+  Widget _buildDetails() {
     final _headlineFont = const TextStyle(fontSize: 22.0);
-    final date = event.dateStart != null
+    final _date = event.dateStart != null
         ? 'vom ${event.dateStart} bis zum ${event.dateEnd}'
         : 'am ${event.dateSingle}';
-    final participants = event.participants.split(RegExp(r"[,;]")).map((s) =>
-        s.trim()).join('\n');
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Details'),
+    final _participants = event.participants
+        .split(RegExp(r"[,;]"))
+        .map((s) => s.trim())
+        .join('\n');
+    return ListView(
+      padding: const EdgeInsets.all(16.0),
+      children: <Widget>[
+        Text(
+          event.title,
+          style: _headlineFont,
         ),
-        body: ListView(
-          padding: const EdgeInsets.all(16.0),
-          children: <Widget>[
-            Text(
-              event.title,
-              style: _headlineFont,
-            ),
-            Text('$date Anmeldeschluss am ${event.registerDate}'),
-            Text(
-              'Beschreibung',
-              style: _headlineFont,
-            ),
-            Text(event.description),
-            Text(event.link),
-            Text(
-              'Kontakt',
-              style: _headlineFont,
-            ),
-            Text(event.organizer),
-            Text(event.phone.isNotEmpty
-                ? event.phone
-                : 'Keine Telefonnummer angegeben'),
-            Text(event.email),
-            Text(
-              'Teilnehmer',
-              style: _headlineFont,
-            ),
-            Text(participants),
-          ],
-        ));
+        Text('$_date Anmeldeschluss am ${event.registerDate}'),
+        Text(
+          'Beschreibung',
+          style: _headlineFont,
+        ),
+        Text(event.description),
+        Text(event.link),
+        Text(
+          'Kontakt',
+          style: _headlineFont,
+        ),
+        Text(event.organizer),
+        Text(event.phone.isNotEmpty
+            ? event.phone
+            : 'Keine Telefonnummer angegeben'),
+        Text(event.email),
+        Text(
+          'Teilnehmer',
+          style: _headlineFont,
+        ),
+        Text(_participants),
+      ],
+    );
   }
 }
